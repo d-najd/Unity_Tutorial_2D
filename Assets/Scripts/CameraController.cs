de0ac18c7e0a1ee76397e6bb84bd5d9df41e2214
najd <dimitar.hristijan.najdovski@gmail.com>
2023-05-13 12:20:29 +0200

    init

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+
+    private void Update()
+    {
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
+    }
+}
