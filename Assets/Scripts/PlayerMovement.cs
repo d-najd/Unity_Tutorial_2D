75edf03cc426508729002908ca6645baebe1f7a0
najd <dimitar.hristijan.najdovski@gmail.com>
2023-05-13 12:56:31 +0200

    Part 5

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,27 @@
-using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D _rigidbody2D;
+    private Rigidbody2D _rb;
     private SpriteRenderer _spriteRenderer;
     private Animator _animator;
     private float _xAxis;
-    [SerializeField] private float jumpVelocity = 12f;
-    [SerializeField] private float moveVelocity = 10f;
+    [FormerlySerializedAs("jumpVelocity")] [SerializeField] private float jumpForce = 12f;
+    [FormerlySerializedAs("moveVelocity")] [SerializeField] private float moveSpeed = 10f;
+    
+    private static readonly int AnimStateTag = Animator.StringToHash("state");
+    private enum AnimationState
+    {
+        Idle = 0,
+        Running = 1,
+        Jumping = 2,
+        Falling = 3,
+    }
 
     private void Start()
     {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
     }
@@ -25,59 +34,39 @@ public class PlayerMovement : MonoBehaviour
         HandleAnimations();
     }
 
-
     private void HandleMovement()
     {
-        _rigidbody2D.velocity = new Vector2(_xAxis * moveVelocity, _rigidbody2D.velocity.y);
+        _rb.velocity = new Vector2(_xAxis * moveSpeed, _rb.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
         {
-            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, jumpVelocity);
+            _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
         }
     }
 
     private void HandleAnimations()
     {
+        var animState = AnimationState.Idle;
         if (_xAxis > 0f)
         {
             _spriteRenderer.flipX = false;
-            //_animator.SetBool(PlayerAnimTag.Running.AsTag(), true);
+            animState = AnimationState.Running;
         }
         else if (_xAxis < 0f)
         {
             _spriteRenderer.flipX = true;
-            //_animator.SetBool(PlayerAnimTag.Running.AsTag(), true);
+            animState = AnimationState.Running;
         }
-        else
+        
+        if (_rb.velocity.y > .1f)
         {
-            //_animator.SetBool(PlayerAnimTag.Running.AsTag(), false);
+            animState = AnimationState.Jumping;
+        } 
+        else if (_rb.velocity.y < -.1f)
+        {
+            animState = AnimationState.Falling;
         }
-    }
-
-}
-
-internal enum AnimationState
-{
-    Idle,
-    Running,
-    Jumping,
-    Falling,
-}
-
-/**
- * Helper method to encapsulate changes to the player animation tags 
- */
- /*
-internal static class PlayerAnimTagExtensions 
-{
-    // TODO compile time checking does not seem to be possible, unit test should be written instead
-    public static string AsTag(this PlayerAnimTag tag)
-    {
-        return tag switch
-        {
-            PlayerAnimTag.Running => "Running",
-            _ => throw new ArgumentOutOfRangeException(nameof(tag), tag, null)
-        };
+        
+        _animator.SetInteger(AnimStateTag, (int) animState);
     }
 }
-*/
